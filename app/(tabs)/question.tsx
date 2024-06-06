4e384385457483e7533c707f8535e6029464149a
Abhinav Devarakonda <abhinavd2015@gmail.com>
2024-06-05 21:39:59 -0500

    no duplicate tab

--- app/(tabs)/question.tsx
+++ app/(tabs)/question.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ThemeProvider, theme } from "react-native-design-system";
-import QuestionPortal from './questionPortal';
-
+import QuestionPortal from '../questionPortal';
 //get it to stop whining
 const components = {};
 
