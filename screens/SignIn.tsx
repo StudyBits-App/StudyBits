e0fa04ffd42f2b66fc0479b3593b0912529818b6
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2024-06-25 20:21:52 -0500

    Fixed android stuff and added github workflow that isn't tested

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -5,7 +5,8 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 
 GoogleSignin.configure({
-    webClientId: '1098397225551-ibat99410nloiruc9g7aecrkvb83ptpf.apps.googleusercontent.com'
+    webClientId: '1098397225551-4fo48u8pni4nct9f1msj5n81nes8b3oe.apps.googleusercontent.com',
+    iosClientId: '1098397225551-ma2bi71anlno8if64vklp33qtce28bi7.apps.googleusercontent.com'
 });
 
 export default function SignIn() {
